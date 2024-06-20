3e7feda3a53bd1faed54c7b0a65fb599f89c9132
redsareraw <dmarshall@musfiber.net>
2024-06-19 21:27:07 -0400

    Created two new files index.html and script.js to showcase learing how to declare variables.

---
+++ lesson6-2/script.js
@@ -0,0 +1,7 @@
+var myFavoriteNumber = 12;
+var myFavoritePet = "dog";
+var rainIsLovely = false;
+var numberOne = 24;
+var numberTwo = 57;
+var addNumbers = numberOne + numberTwo;
+var divideNumbers = numberOne / numberTwo;
